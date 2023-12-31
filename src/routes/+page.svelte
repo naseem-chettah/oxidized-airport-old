99b12272d599a9e70ddf570cf6db4684ffd9a48c
seth-ossidian <naz.zandpm@gmail.com>
2023-12-31 13:04:36 +0100

    let's work on the front end

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,13 @@
 <script>
-  import { invoke } from '@tauri-apps/api/tauri';
-
-  let title = '';
-  let firstName = '';
-  let lastName = '';
-
-  async function run_surreal() {
-    await invoke('run_surreal', { title, firstName, lastName })
-  }
+  //import { invoke } from "@tauri-apps/api/tauri";
 </script>
 
-<form on:submit={run_surreal}>
-  <input type="text" placeholder="first name" bind:value={firstName}>
-  <input type="text" placeholder="last name" bind:value={lastName}><br>
-  <input type="text" placeholder="title" bind:value={title}>
-  <input type="submit">
-</form>
+<main>
+</main>
+
+<style>
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+</style>
