a4e13feedbd5444dcff410bdc1caa2a872266d02
seth-ossidian <naz.zandpm@gmail.com>
2023-12-31 13:16:59 +0100

    header created

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <main>
+  <p>So I think I’ve talked before about this school project but in case I didn’t it was supposed to be creating a simple data base setup for a hypothetical airport (I have no damn idea if that how you spell that word but yeah it sounds about right to me!). But you know me, I love to over-complicate shit. So now I’m basically working on an across platform front-end for this DB, which btw I’ll be using the SurrealDB DBMS for.</p>
 </main>
 
 <style>
