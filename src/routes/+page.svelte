b4738418d0c9d9dd60271e5567d77c8401a59d14
seth-ossidian <naz.zandpm@gmail.com>
2023-12-31 14:16:08 +0100

    tab system has been added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,20 @@
   //import { invoke } from "@tauri-apps/api/tauri";
   import Header from "../lib/components/Header.svelte";
   import Footer from "../lib/components/Footer.svelte";
+  import Tabs from "../lib/components/Tabs.svelte";
+
+  //tabs
+  let items = ["Passengers", "Flights", "Airports"];
+  let activeItem = "Passengers";
+
+  const tabChange = (e) => {
+    activeItem = e.detail;
+  }
 </script>
 
 <Header />
 <main>
+  <Tabs {activeItem} {items} on:tabChange={tabChange} />
 </main>
 <Footer />
 
