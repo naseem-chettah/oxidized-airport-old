e3f89017ecc7625bf88c427f60761dd53d63cf12
seth-ossidian <naz.zandpm@gmail.com>
2024-01-02 10:26:46 +0100

    button component added

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let type = 'primary';
+  export let flat = false;
+  export let inverse = false;
+</script>
+
+<button class="{type}">
+  <slot></slot>
+</button>
+
+<style>
+  button {
+    border: 0;
+    cursor: pointer;
+    border-radius: 6px;
+    padding: 8px 12px;
+    font-weight: bold;
+    box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
+  }
+</style>
